d7df6ce7ed426ede1e28e910476a1f361b13b3e5
Alec Wolyniec <alecwolyniec@gmail.com>
2021-01-21 17:56:35 -0500

    feat: Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Search for Treasure
+
+Just download and open index.html in your browser to get started!
---
+++ script.js
@@ -0,0 +1,144 @@
+class Coin {
+  constructor(topPx, leftPx) {
+    return {
+      filename: "coin_2.png",
+      top: `${topPx}px`,
+      left: `${leftPx}px`,
+      width: "70px",
+      points: 1,
+      whatYouveFound: "a coin"
+    };
+  }
+}
+
+class Diamond {
+  constructor(topPx, leftPx) {
+    return {
+      filename: "diamond5.png",
+      top: `${topPx}px`,
+      left: `${leftPx}px`,
+      width: "90px",
+      points: 10,
+      whatYouveFound: "a diamond"
+    };
+  }
+}
+
+class Goblet {
+  constructor(topPx, leftPx) {
+    return {
+      filename: "goblet.png",
+      top: `${topPx}px`,
+      left: `${leftPx}px`,
+      width: "140px",
+      points: 100,
+      whatYouveFound: "a goblet"
+    };
+  }
+}
+
+class TreasureChest {
+  constructor(topPx, leftPx) {
+    return {
+      filename: "treasure_chest.png",
+      top: `${topPx}px`,
+      left: `${leftPx}px`,
+      width: "100px",
+      points: 1000,
+      whatYouveFound: "a treasure chest"
+    };
+  }
+}
+
+class Utah {
+  constructor(topPx, leftPx) {
+    return {
+      filename: "utah.png",
+      top: `${topPx}px`,
+      left: `${leftPx}px`,
+      width: "120px",
+      points: 300,
+      whatYouveFound: "Utah"
+    };
+  }
+}
+
+const layouts = [
+  [
+    new Coin(40, 30),
+    new Coin(400, 300),
+    new Coin(264, 800),
+    new Coin(500, 300),
+    new Coin(464, 800),
+    new Coin(400, 1200),
+    new Coin(300, 1200),
+    new Coin(500, 1164),
+    new Coin(500, 164),
+    new Diamond(350, 19),
+    new Diamond(20, 200),
+    new Diamond(150, 1100),
+    new Goblet(60, 700),
+    new TreasureChest(400, 700),
+    new Utah(0, 1200),
+  ]
+];
+
+function startNewGame() {
+  // remove any existing objects from a previous game
+  const objectsContainer = document.getElementById("objects-container");
+  objectsContainer.innerHTML = "";
+  const pointsNode = document.getElementById("points");
+  pointsNode.innerHTML = "Points: 0";
+  const foundSomethingNode = document.getElementById("found-something");
+  foundSomethingNode.innerHTML = "";
+
+  const randomLayoutIndex = Math.floor(Math.random() * layouts.length);
+  const randomLayout = layouts[randomLayoutIndex];
+
+  for (let item of randomLayout) {
+    const { filename, top, left, width, points, whatYouveFound } = item;
+    const objectNode = document.createElement("img");
+    objectNode.classList.add("object-to-find");
+    objectNode.classList.add("cursor-enabled");
+    objectNode.src = `assets/${filename}`;
+    objectNode.style.top = top;
+    objectNode.style.left = left;
+    if (width) {
+      objectNode.style.width = width;
+    }
+    objectNode.onclick = function(event) {
+      event.preventDefault();
+      event.stopPropagation();
+      objectNode.style.opacity = 1;
+      objectNode.classList.remove("cursor-enabled");
+      objectNode.onclick = null;
+
+      const currentTotalPoints = pointsNode.innerHTML.match(/\d+/)[0];
+      const newTotalPoints = Number(currentTotalPoints) + points;
+      pointsNode.innerHTML = `Point${newTotalPoints == 1 ? '' : 's'}: ${newTotalPoints}`;
+      foundSomethingNode.innerHTML = `You found ${whatYouveFound}!`;
+    }
+    objectsContainer.appendChild(objectNode);
+  }
+}
+
+window.onload = function() {
+  document.getElementById("new-game-button").onclick = startNewGame;
+  startNewGame();
+
+  setTimeout(function() {
+    const hint1Node = document.getElementById("hint-1");
+    hint1Node.style.display = "block";
+    hint1Node.onclick = function () {
+      hint1Node.remove();
+    }
+  }, 10 * 1000);
+
+  setTimeout(function() {
+    const hint2Node = document.getElementById("hint-2");
+    hint2Node.style.display = "block";
+    hint2Node.onclick = function() {
+      hint2Node.remove();
+    }
+  }, 30 * 1000);
+}
